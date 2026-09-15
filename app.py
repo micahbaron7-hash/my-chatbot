fabe5aa6b09383fda20066cb1e8c9fffcffe0add
micahbaron7-hash <micahbaron7@gmail.com>
2026-09-14 21:55:55 -0400

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ import io
 import threading
 import math
 
-gamble_bp = Blueprint("gamble", __name__)
 
 app = Flask(__name__)
 app.secret_key = os.environ.get("FLASK_SECRET_KEY", "studyspace-secret-key")
